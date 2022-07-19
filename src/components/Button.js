6a313a759742fabc09bc739cc758b47e18193eb3
Ryan Parappuram <71271657+rparappuram@users.noreply.github.com>
2022-07-19 13:08:25 -0700

    uploading files

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  color: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default Button;
